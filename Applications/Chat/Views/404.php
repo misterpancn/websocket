15ec7735ccd41d3ebb8f7c1fca2247796f427094
buck <misterpancn@gmail.com>
2018-09-15 12:11:54 +0800

    add file

---
+++ Applications/Chat/Views/404.php
@@ -0,0 +1,46 @@
+<html xml:lang="eng" lang="eng">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="author" content="Yigit Yigit Ce.[pulyavserdce.com]" />
+    <meta name="description" content="Site description" />
+    <meta name="keywords" content="keywords, keyword, seo, google" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <title>Error : 404</title>
+    <link rel="stylesheet" media="screen" href="https://www.misterpan.cn/_css/style.css" type="text/css" />
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.png" />
+    <link rel="icon" type="image/x-icon" href="favicon.png" />
+    <link rel="apple-touch-icon" href="favicon.png" />
+    <link rel="apple-touch-startup-image" href="favicon.png" />
+</head>
+<body>
+<div class="controller">
+    <div class="objects">
+        <!-- text area -->
+        <div class="text-area rotate">
+            <p class="error">Error 404</p>
+            <p class="details">There was a problem<br /><br />The page you are looking for is not here or moved.</p>
+        </div>
+        <!-- text area -->
+        <!-- home page -->
+        <div class="copyrights">Collect from <a href="https://chat.misterpan.cn/" >Website Template</a></div>
+        <div class="homepage rotate">
+            <a href="/">Back to homepage</a>
+        </div> <!-- home page -->
+    </div> <!-- social-icons -->
+    <div class="social">
+        <ul class="social-icons">
+            <li><a href="/"><img src="https://www.misterpan.cn/_images/forrst.png" alt="Forrst" /></a></li>
+            <li><a href="/"><img src="https://www.misterpan.cn/_images/dribbble.png" alt="Dribbble" /></a></li>
+            <li><a href="/"><img src="https://www.misterpan.cn/_images/deviantart.png" alt="DeviantArt" /></a></li>
+            <li><a href="/"><img src="https://www.misterpan.cn/_images/flickr.png" alt="Flickr" /></a></li>
+            <li><a href="/"><img src="https://www.misterpan.cn/_images/twitter.png" alt="Twitter" /></a></li>
+            <li><a href="/"><img src="https://www.misterpan.cn/_images/facebook.png" alt="Facebook" /></a></li>
+            <li><a href="/"><img src="https://www.misterpan.cn/_images/skype.png" alt="Skype" /></a></li>
+        </ul>
+    </div>
+    <!-- social-icons -->
+</div>
+
+</body>
+</html>
