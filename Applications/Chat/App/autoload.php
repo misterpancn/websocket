ba2741554719697e175b9148847c53981d99d5d1
buckpan <buck.pan@szyuxuan.com>
2018-12-11 14:26:01 +0800

    optimized

--- Applications/Chat/App/autoload.php
+++ Applications/Chat/App/autoload.php
@@ -1,23 +1,21 @@
 <?php
-ini_set('date.timezone','Asia/Shanghai');
-ini_set('display_errors','on');
+ini_set('date.timezone', 'Asia/Shanghai');
+ini_set('display_errors', 'on');
 error_reporting(E_COMPILE_ERROR);
 session_start();
 require_once __DIR__ . '/../../../vendor/autoload.php';
 
 use GatewayClient\Gateway;
+
 // 加载所有Applications/Chat/App/Controllers/System所有文件
-foreach(glob(__DIR__.'/Controllers/System/*.php') as $start_file)
-{
+foreach (glob(__DIR__ . '/Controllers/System/*.php') as $start_file) {
     require_once $start_file;
 }
-if(!extension_loaded('pcntl'))
-{
+if (!extension_loaded('pcntl')) {
     exit("Please install pcntl extension. See http://doc3.workerman.net/appendices/install-extension.html\n");
 }
 
-if(!extension_loaded('posix'))
-{
+if (!extension_loaded('posix')) {
     exit("Please install posix extension. See http://doc3.workerman.net/appendices/install-extension.html\n");
 }
 Gateway::$registerAddress = REGISTER_SERVER;
@@ -31,23 +29,25 @@ $redisService = new \Predis\Client(['host' => '127.0.0.1', 'port' => '6379']);
 /**
  * 设置登录的token   防止非法登录
  */
-if(isset($_SESSION['token']) && $_SESSION['token'] && $_SESSION['users_id']){
+if (isset($_SESSION['token']) && $_SESSION['token'] && $_SESSION['users_id']) {
     $all_Token = $globalServer->allToken;
-    if(empty($all_Token) || sizeof($all_Token) == 0 || !in_array($_SESSION['token'],$all_Token)){
-        $_SESSION['token'] = md5(time().$_SESSION['users_id']);
+    if (empty($all_Token) || sizeof($all_Token) == 0 || !in_array($_SESSION['token'], $all_Token)) {
+        $_SESSION['token'] = md5(time() . $_SESSION['users_id']);
         $overflow = 0;
-        do
-        {
+        do {
             $old_value = $new_value = $all_Token;
             $new_value[] = $_SESSION['token'];
             $overflow++;
-        }
-        while(!$globalServer->cas('allToken', $old_value, $new_value) && $overflow < 10);
+        } while (!$globalServer->cas('allToken', $old_value, $new_value) && $overflow < 10);
     }
 }
 /**
  * 用户未登录且不是在login页面  重定向至登录页
  */
-if(empty($_SESSION['users_id']) && Gateway::isUidOnline($_SESSION['users_id'])==0 && !preg_match('/login.php/',$_SERVER['REQUEST_URI']) && !preg_match('/register.php/',$_SERVER['REQUEST_URI']) && !preg_match('/StartCaptchaServlet.php/',$_SERVER['REQUEST_URI']) && !preg_match('/VerifyLoginServlet.php/',$_SERVER['REQUEST_URI'])){
+$pageName = substr($_SERVER['REQUEST_URI'], 1, (strpos($_SERVER['REQUEST_URI'], '.') - 1));
+$authorizations = [
+    'login', 'register', 'StartCaptchaServlet', 'VerifyLoginServlet', 'api',
+];
+if (empty($_SESSION['users_id']) && Gateway::isUidOnline($_SESSION['users_id']) == 0 && !in_array($pageName, $authorizations)) {
     redirect('login.php');
 }
---
+++ Applications/Chat/Views/api.php
@@ -0,0 +1,24 @@
+<?php
+require_once "../App/autoload.php";
+$action = str_tirm($_GET['action']);
+switch ($action){
+    case 'refresh_close':
+        $uid = str_tirm($_POST['uid']);
+        $all_user = $globalServer->allUsers;
+        if(in_array($uid,$all_user)){
+            echo 'is_online';
+        }else{
+            echo 'is_offline';
+        }
+        exit;
+        break;
+    case 'get_emoji':
+        $res = $globalDB->select(['path','phrase','common'])->from('emoji')->query();
+        foreach ($res as $k=>$v){
+            $res[$k]['url'] = $v['path'];
+            $res[$k]['icon'] = $v['path'];
+        }
+        echo json_encode($res);
+        exit;
+        break;
+}
